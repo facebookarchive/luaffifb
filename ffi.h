9fa458ab2a5ecf9e9ae706affa214dfe563f35ab
Sam Gross <sgross@devgpu037.prn2.facebook.com>
2015-04-23 17:11:31 -0700

    Assume complex number support for now.
    Luarocks doesn't compile with std=c99.

--- ffi.h
+++ ffi.h
@@ -58,11 +58,9 @@ extern "C" {
 #include <sys/mman.h>
 #endif
 
-#if __STDC_VERSION__+0 >= 199901L
 #include <complex.h>
 #define HAVE_COMPLEX
 #define HAVE_LONG_DOUBLE
-#endif
 
 #ifndef NDEBUG
 #define DASM_CHECKS
