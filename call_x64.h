666bb413a5333c8a05a30f6cef01368011a3f316
Sam Gross <colesbury@gmail.com>
2015-09-16 16:57:32 -0400

    Fix LICENSE and PATENTS file

--- call_x64.h
+++ call_x64.h
@@ -10,8 +10,8 @@
 #endif
 
 /* vim: ts=4 sw=4 sts=4 et tw=78
- * Copyright (c) 2015-present, Facebook, Inc. All rights reserved.
- * Copyright (c) 2011 James R. McKaskill.
+ * Portions copyright (c) 2015-present, Facebook, Inc. All rights reserved.
+ * Portions copyright (c) 2011 James R. McKaskill.
  *
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree. An additional grant
